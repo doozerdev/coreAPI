c5a16f35136b5f3534a1ec105d0b53d6c8701d9f
Joe Basirico <joebasirico@gmail.com>
2014-12-29 13:25:29 -0700

    RSpec login and logout tests added to login_spec.rb

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module CoreAPI
 
     config.generators do |g|
       g.orm :mongo_mapper
+      g.test_framework :rspec, :fixture => true, :views => false
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -1,11 +1,17 @@
 require "rails_helper"
 require "spec_helper"
 
+MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017)
+MongoMapper.database = "doozer_development"
+MongoMapper.database.collections.each { |c| c.drop_indexes }
+
+session_id = nil
+
 RSpec.describe "SessionsController" do
   describe "GET /api/login/:oauth_token" do
     it "logs in a user with a FB token" do
 
-      fb_oauth_token = 'CAAU9WC52BFcBAOnASMkwnaTNhcbYHibhTLV7eGu4Kq5aTaeeh9JHW9f7SqPrJKDcJQgE8Pg5GSG8f3zVPqtIlzR21rpqwv6flZBxnzXpvpYBnpLIAgx3ZBPZAtbEhgQJB6SWBXP04IFH02bf6iZAZCcxRZCSIgJvEfxztQd2cor0LVBtlLGxFZC2XnG3LU2sVFVVnBTD0qxLKGWkyZC0bikJ'
+      fb_oauth_token = 'CAAU9WC52BFcBADYVzRsGquuWlt45fkR6xWR63TUhO35OEGyLUhAdp07Nb38vO7uDJzdZAHqEKXpmGT6JHubHvHlgYVUTzWCZCdAK1RwVQlEDSNDF0uW0CWCMpIB6cC5K4H1SENgHfNgeYab8VEcdmL5JSNJLPznY6NtukZBd9uBJPVlxOK3IQXs9jJaSNVcRBccXHqSwPXre50h1yBp'
 
       get "/api/login/#{fb_oauth_token}", {}, { "Accept" => "application/json" }
 
@@ -14,8 +20,20 @@ RSpec.describe "SessionsController" do
       expect(response.status).to eq 201
 
       session_id = JSON.parse(response.body)['session_id']
-      
+
       expect(session_id).to match(/[0-9a-f]{32}/)
     end
   end
+
+  describe "DELETE /api/logout" do
+    it "logs out one user" do
+      expect(session_id).to  match(/[0-9a-f]{32}/)
+
+      delete "/api/logout", {}, {"HTTP_SESSION_ID" => session_id}
+
+      expect(response.status).to eq 200
+
+    end
+  end
+
 end
