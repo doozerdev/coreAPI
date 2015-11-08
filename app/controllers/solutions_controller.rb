3dfd8c8131e6062becdb3dc0ce8734bfcac69fa4
Joe Basirico <joebasirico@gmail.com>
2015-11-08 10:42:03 -0800

    Revert "update to add date_link_updated on item when requested from solution"
    This reverts commit d0fc3a854be67a16cc26eb42063ae0bb94e61bc1.

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -63,11 +63,7 @@ class SolutionsController < BaseApiController
 
   def items
     solutionsList = ItemSolutionMap.where(:solution_id => params[:id], :linked => true)
-    itemsList     = solutionsList.collect { |s| 
-      i = Item.where(:id => s.item_id).first 
-      i['date_link_updated'] = s.updated_at
-      i
-    }
+    itemsList     = solutionsList.collect { |s| Item.where(:id => s.item_id).first }
     render json: { items: itemsList }, :status => :ok
   end
 
